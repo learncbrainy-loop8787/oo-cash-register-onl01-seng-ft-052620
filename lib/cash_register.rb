da401b2f70293aae280b6e197fb85cd49a289aa6
korey ko <koreykendrick1@gmail.com>
2020-06-06 14:39:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   end 
 def add_item(title,price,quantity = 1)
   self.total += price * quantity
-    if quantity > 1
+    if quantity > 0
       i = 0
       while i < quantity
         @items << title
