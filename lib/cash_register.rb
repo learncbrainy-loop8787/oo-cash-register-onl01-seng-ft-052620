b8253bf4f141e241613fbd12e47c1076e1020080
korey ko <koreykendrick1@gmail.com>
2020-06-06 14:24:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount, :last_transaction_amount, :items
+  
+  def initialize(discount = 0)
+  @total = 0
+  @discount = discount
+  @items = []
+  end 
+def add_item(title,price,quantity = 1)
+    if quantity > 1
+      i = 0
+      while i < quantity
+        @items << title
+        i += 1
+      end
+    end
+end
+end
